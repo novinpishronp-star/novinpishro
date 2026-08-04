374774eab49c1bb7560721e65103127d23bf3983
novinpishronp-star <novinpishronp@gmail.com>
2026-08-04 15:12:55 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Novin Pishro
+window.addEventListener("load",()=>{
+
+    const buttons=document.querySelectorAll(".glass-btn");
+
+    buttons.forEach((btn,index)=>{
+
+        setTimeout(()=>{
+
+            btn.classList.add("show");
+
+        },700+(index*90));
+
+    });
+
+});
