2cc9cf6d8642c3e5e1e6fbf9315f0cd10d5a9f25
novinpishronp-star <novinpishronp@gmail.com>
2026-08-04 21:29:51 +0330

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,32 @@ window.addEventListener("load", () => {
     });
 
 });
+document.querySelectorAll(".glass-btn").forEach(button=>{
+
+    button.addEventListener("click",function(e){
+
+        const circle=document.createElement("span");
+
+        circle.classList.add("ripple");
+
+        const rect=this.getBoundingClientRect();
+
+        const size=Math.max(rect.width,rect.height);
+
+        circle.style.width=size+"px";
+        circle.style.height=size+"px";
+
+        circle.style.left=(e.clientX-rect.left-size/2)+"px";
+        circle.style.top=(e.clientY-rect.top-size/2)+"px";
+
+        this.appendChild(circle);
+
+        setTimeout(()=>{
+
+            circle.remove();
+
+        },600);
+
+    });
+
+});
